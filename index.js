e7ff5bcc2b0f21457cebcc74678d754695384430
Jesús Mejía <chucho4720013@gmail.com>
2021-10-25 22:44:54 -0500

    Introduction (#1)
    * Setting environment
    
    * create server
    
    * Routing

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.get('/products', (req, res) => {
+  res.json({ hello: 'Hello world'})
+})
+
+app.listen(port, () =>{
+  console.log('listening in port ' + port);
+})
