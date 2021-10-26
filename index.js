3d526ac85b15296ea987df1a2fd46777f93dafbb
Jesus Mejia <chucho4720013@gmail.com>
2021-10-25 23:06:34 -0500

    Query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 
 const app = express();
 const port = 3000
@@ -8,7 +9,47 @@ app.get('/', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json({ hello: 'Hello world'})
+  const products = []
+  const { size } = req.query
+  const limit = size || 10
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+    
+  }
+  res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params
+  res.json({
+    id,
+    name: 'products',
+    price: 821349
+  })
+})
+
+app.get('/category/:categoryId/product/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+  res.json({
+    categoryId,
+    productId,
+  })
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query
+  if(limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay ningun valor')
+  }
 })
 
 app.listen(port, () =>{
